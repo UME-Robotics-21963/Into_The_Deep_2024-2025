5dd8c80cef482fa4e4cc9e57ec57a9befdd10b6a
FTC-21963 <theMultiversalCoders21963@outlook.com>
2024-10-02 15:59:50 -0500

    Setup Teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
@@ -7,15 +7,13 @@ public class BasicTeleOp extends Hardware {
     @Override
     public void init() {
         Initialize(hardwareMap);
-
-        frontRight.setPower(gamepad1.left_stick_y);
-        backRight.setPower(gamepad1.left_stick_y);
-        backLeft.setPower(gamepad1.left_stick_y);
-        frontLeft.setPower(gamepad1.left_stick_y);
     }
 
     @Override
     public void loop() {
-
+        frontRight.setPower(gamepad1.left_stick_y);
+        backRight.setPower(gamepad1.left_stick_y);
+        backLeft.setPower(gamepad1.left_stick_y);
+        frontLeft.setPower(gamepad1.left_stick_y);
     }
 }
