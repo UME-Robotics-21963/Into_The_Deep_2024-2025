1053066a27d2010d212ad1a5f17a6f4729a971fc
FTC-21963 <theMultiversalCoders21963@outlook.com>
2024-10-18 16:33:35 -0500

    -Added Arm Stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
@@ -13,9 +13,21 @@ public class BasicTeleOp extends Hardware {
     public void loop() {
         double leftSidePower = gamepad1.left_stick_y - gamepad1.right_stick_x;
         double rightSidePower = gamepad1.left_stick_y + gamepad1.right_stick_x;
-        leftSide.setPower(leftSidePower);
-        rightSide.setPower(rightSidePower);
+        double armPower = 0;
 
+        if(gamepad1.right_trigger != 0)
+            armPower = 1;
+        if(gamepad1.left_trigger != 0)
+            armPower = -1;
+        if(gamepad1.right_trigger + gamepad1.left_trigger != 0)
+            armPower = 0;
+
+
+        leftSide.setPower(leftSidePower * .5);
+        rightSide.setPower(rightSidePower * .5);
+        arm.setPower(armPower * .5);
+
+
+        }
     }
-}
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -2,22 +2,28 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 // A required section to write all of your code
 public abstract class Hardware extends OpMode {
     // Makes an instance of DCMotor to create copies
-    public static DcMotor rightSide, leftSide;
+    public static DcMotor rightSide, leftSide, arm;
 
     // Creates an area to change the copied class
     public static void Initialize(HardwareMap hMap) {
         // Gets the port of motor from phone to set this instance to that port
         rightSide = hMap.get(DcMotor.class, "rightSide");
         leftSide = hMap.get(DcMotor.class, "leftSide");
+        arm = hMap.get(DcMotor.class, "arm");
+
 
         // Sets the direction of motor
-        rightSide.setDirection(DcMotor.Direction.FORWARD);
-        leftSide.setDirection(DcMotor.Direction.REVERSE);
+        rightSide.setDirection(DcMotor.Direction.REVERSE);
+        leftSide.setDirection(DcMotor.Direction.FORWARD);
+        arm.setDirection(DcMotor.Direction.FORWARD);
+
+
 
 
         rightSide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
